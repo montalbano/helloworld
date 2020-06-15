2bda7694da90e2b24564cde1e15e3fc9fd62c691
gianfranco <vecchio@lns.infn.it>
2020-06-15 09:29:11 +0200

    test page

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 var express = require('express');
+const path = require('path');
+const router = express.Router();
 var app = express();
-app.use(express.static(__dirname + '/public')); //__dir and not _dir
+//app.use(express.static(__dirname + '/public')); //__dir and not _dir
 var port = 3333; // you can use any port
+
+
+router.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/public/index.html'));
+});
+
+router.get('/test',function(req,res){
+    res.sendFile(path.join(__dirname+'/public/test.html'));
+});
+
+
+app.use('/', router);
 app.listen(port);
 console.log('Server listen on port ' + port);
+
+
+
